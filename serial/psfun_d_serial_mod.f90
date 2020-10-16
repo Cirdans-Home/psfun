4149fc9990b8ea1beb1381176473faa6c4e1acff
Cirdans-Home <f.durastante@na.iac.cnr.it>
2020-10-16 15:08:58 +0200

    Corrected matrix creation for GENPADE and added function for sparse matrix argument

--- serial/psfun_d_serial_mod.f90
+++ serial/psfun_d_serial_mod.f90
@@ -47,7 +47,9 @@ module psfun_d_serial_mod
         procedure, pass(fun) :: setinteger => psfun_d_setinteger
         generic, public :: set => setstring, setreal, setinteger
         ! Computes the function
-        procedure, pass(fun) :: apply      => psfun_d_serial_apply
+        procedure, pass(fun) :: applya      => psfun_d_serial_apply_array
+        procedure, pass(fun) :: applys      => psfun_d_serial_apply_sparse
+        generic, public :: apply => applya, applys
     end type psfun_d_serial
 
     private :: psfun_d_setstring, psfun_d_setreal, psfun_d_setinteger
@@ -116,7 +118,7 @@ contains
 
     end subroutine psfun_d_setinteger
 
-    subroutine psfun_d_serial_apply(fun,a,y,x,info)
+    subroutine psfun_d_serial_apply_array(fun,a,y,x,info)
         ! This is the core of the function apply on a serial matrix to compute
         ! :math:`y = f(\alpha*A) x`. It calls on the specific routines
         ! implementing the different functions. It is the function to modify if
@@ -137,7 +139,7 @@ contains
         real(psb_dpk_), allocatable     :: fA(:,:),wsp(:)
         integer(psb_ipk_), allocatable  :: ipiv(:)
         ! local constants
-        integer(psb_ipk_)       :: err_act
+        integer(psb_ipk_)       :: err_act, i, j
         character(len=20)       :: name
 
         info = psb_success_
@@ -213,7 +215,11 @@ contains
                     goto 9999
                 end if
 
-                fA = reshape(wsp, shape=shapes)
+                do i = 1, n, 1
+                    do j = 1, m, 1
+                        fA(i,j) = wsp(iexph+(j-1)*m+i-1)
+                    end do
+                end do
                 y = matmul(fA,x)
 
                 if (allocated(wsp)) deallocate(wsp, stat=info)
@@ -255,6 +261,51 @@ contains
 
         return
 
-    end subroutine
+    end subroutine psfun_d_serial_apply_array
+
+    subroutine psfun_d_serial_apply_sparse(fun,a,y,x,info)
+        ! This is the core of the function apply on a serial matrix to compute
+        ! :math:`y = f(\alpha*A) x` when A is memorized in a sparse storage.
+        ! In this case the routine converts it to a dense storage and then calls
+        ! the array version of itself. That is the one implementing the
+        ! different functions. It is the function to modify if ones want to
+        ! interface a new function that was not previously available or a new
+        ! algorithm (variant) for an already existing function.
+        use psb_base_mod
+        implicit none
+
+        class(psfun_d_serial), intent(inout) :: fun  ! Function information
+        type(psb_dspmat_type), intent(inout) :: a    ! Matrix
+        real(psb_dpk_), intent(in)           :: x(:) ! Input vector
+        real(psb_dpk_), intent(out)          :: y(:) ! Output vector
+        integer(psb_ipk_), intent(out)       :: info ! Information on the output
+
+        ! local variables
+        real(psb_dpk_), allocatable :: amat(:,:)
+        integer(psb_ipk_) :: n,nnz,i
+        type(psb_d_coo_sparse_mat)  :: acoo
+
+        call a%a%cp_to_coo(acoo,info)
+        if(info /= psb_success_) write(psb_err_unit,*)"Error in Copy to COO"
+
+        n = a%get_nrows()
+        nnz = acoo%get_nzeros()
+
+        allocate(amat(n,n), stat=info)
+        if (info /= 0) write(psb_err_unit,*)"amat: Allocation request denied"
+
+        amat = 0_psb_dpk_
+        do i = 1, nnz, 1
+            amat(acoo%ia(i),acoo%ja(i)) = acoo%val(i)
+        end do
+
+        call fun%apply(amat,y,x,info)
+
+        if (allocated(amat)) deallocate(amat, stat=info)
+        if (info /= 0) write(psb_err_unit,*)"amat: Deallocation request denied"
+
+        return
+
+    end subroutine psfun_d_serial_apply_sparse
 
 end module psfun_d_serial_mod
