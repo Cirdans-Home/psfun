c74a2b2b226fb4ab50b4d6f1331b8df0fdab79cd
Cirdans-Home <f.durastante@na.iac.cnr.it>
2020-10-20 15:39:56 +0200

    Added utils for elliptic integrals/jacobi functions, test and empty quadrature module

---
+++ examples/utils/utiltest.f90
@@ -0,0 +1,81 @@
+! BSD 3-Clause License
+!
+! Copyright (c) 2020, Fabio Durastante
+! All rights reserved.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions are met:
+!
+! 1. Redistributions of source code must retain the above copyright notice, this
+!    list of conditions and the following disclaimer.
+!
+! 2. Redistributions in binary form must reproduce the above copyright notice,
+!    this list of conditions and the following disclaimer in the documentation
+!    and/or other materials provided with the distribution.
+!
+! 3. Neither the name of the copyright holder nor the names of its
+!    contributors may be used to endorse or promote products derived from
+!    this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+! DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+! FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+! DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+! SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+! OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+! OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+program utiltest
+
+  use psb_base_mod
+  use psfun_utils_mod
+  implicit none
+
+  ! blacs parameters
+  integer(psb_ipk_)           :: ictxt, iam, np
+  ! flags
+  integer(psb_ipk_)           :: info
+  ! Variables
+  real(psb_dpk_)              :: K(2),L,u,sn,cn,dn
+
+  info=psb_success_
+  call psb_init(ictxt)
+  call psb_info(ictxt,iam,np)
+
+  if (iam < 0) then
+    call psb_exit(ictxt) ! This should not happen, but just in case
+    stop
+  endif
+  if(psb_get_errstatus() /= 0) goto 9999
+
+  if( (iam == 0).and.(np > 1) ) then
+    write(psb_err_unit, *) "This is a serial example, number of processes is ",np
+    goto 9999
+  else if( (iam == 0).and.(np == 1)) then
+    write(psb_out_unit,*) "Welcome to the utilstest program of PSFUN"
+  end if
+
+  ! Test of Elliptic Jacobi Integrals and Functions
+  write(psb_out_unit,*)
+  write(psb_out_unit,'("Elliptic Integrals and Jacobi Functions")')
+  L = 0.5_psb_dpk_
+  K = ellipkkp(L)
+  write(psb_out_unit,'("L = ",f3.1," K = ",f17.15," Kp = ",f17.15)')L,K(1),K(2)
+  write(psb_out_unit,'("L = 0.5 K = 1.588191701877384 Kp = 2.978718299395645 (Control Values)")')
+  write(psb_out_unit,'("  Jacobi Elliptic Functions")')
+  u = 1.0_psb_dpk_
+  L = 0.5_psb_dpk_
+  call ellipj(u,L,sn,cn,dn)
+  write(psb_out_unit,'("sn = ",f17.15," cn = ",f17.15," dn = ",f17.15)')sn,cn,dn
+  write(psb_out_unit,'("sn = 0.838274911024583 cn = 0.545247809300255 dn = 0.984699634947678 (Control Values)")')
+
+  call psb_exit(ictxt)
+  stop
+
+9999 call psb_error(ictxt)
+
+  stop
+
+end program utiltest
---
+++ utils/psfun_d_utils_mod.f90
@@ -0,0 +1,118 @@
+! BSD 3-Clause License
+!
+! Copyright (c) 2020, Fabio Durastante
+! All rights reserved.
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions are met:
+!
+! 1. Redistributions of source code must retain the above copyright notice, this
+!    list of conditions and the following disclaimer.
+!
+! 2. Redistributions in binary form must reproduce the above copyright notice,
+!    this list of conditions and the following disclaimer in the documentation
+!    and/or other materials provided with the distribution.
+!
+! 3. Neither the name of the copyright holder nor the names of its
+!    contributors may be used to endorse or promote products derived from
+!    this software without specific prior written permission.
+!
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+! DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+! FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+! DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+! SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+! OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+! OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+submodule (psfun_utils_mod) psfun_d_utils_mod
+  use psb_base_mod
+  implicit none
+
+contains
+
+  module function ellipkkp(L) result(K)
+    ! Complete elliptic integral of the first kind, with complement.
+    ! Returns the value of the complete elliptic integral of the first kind,
+    ! evaluated at :math:`M=\exp(-2\pi L)`, :math:`0 < L < \infty`, and the
+    ! complementarity parameter :math:`1-M`.
+    use psb_base_mod
+    implicit none
+
+    real(psb_dpk_), intent(in)  :: L
+    real(psb_dpk_)              :: K(2)
+
+    ! Local variables
+    real(psb_dpk_)    :: M,a0,a1,b0,b1,c1,w1,s0,MM
+    integer(psb_ipk_) :: i1
+
+    ! When M=exp(-2 π L) ≈ 0, use an O(M) approximation
+    if(L > 10) then
+      K(1) = DPI/2_psb_dpk_
+      K(2) = DPI*L + log(4.0_psb_dpk_)
+      return
+    end if
+
+    M = exp(-2.0_psb_dpk_*DPI*L)
+    a0 = 1.0_psb_dpk_
+    b0 = sqrt(1-M)
+    s0 = M
+    i1 = 0
+    MM = 1.0_psb_dpk_
+    do while (MM > EPSILON(MM) )
+      a1 = (a0+b0)/2.0_psb_dpk_
+      b1 = sqrt(a0*b0)
+      c1 = (a0-b0)/2.0_psb_dpk_
+      i1 = i1 + 1
+      w1 = (2.0_psb_dpk_**i1)*(c1**2)
+      MM = w1
+      s0 = s0 + w1
+      a0 = a1
+      b0 = b1
+    end do
+    K(1) = DPI/(2.0_psb_dpk_*a1)
+
+    a0 = 1.0_psb_dpk_
+    b0 = sqrt(M)
+    s0 = 1-M
+    i1 = 0
+    MM = 1.0_psb_dpk_
+    do while (MM > EPSILON(MM) )
+      a1 = (a0+b0)/2.0_psb_dpk_
+      b1 = sqrt(a0*b0)
+      c1 = (a0-b0)/2.0_psb_dpk_
+      i1 = i1 + 1
+      w1 = (2.0_psb_dpk_**i1)*(c1**2)
+      MM = w1
+      s0 = s0 + w1
+      a0 = a1
+      b0 = b1
+    end do
+    K(2) = DPI/(2.0_psb_dpk_*a1)
+
+
+    return
+
+  end function ellipkkp
+
+  module subroutine d_ellipj(u,L,sn,cn,dn)
+    ! Returns the values of the Jacobi elliptic functions evaluated at double
+    ! argument u and parameter :math:`M = \exp(-2 \pi L)`, :math:`0 < L < \infty`.
+    ! For :math:`M = K^2`, and `K` the elliptic modulus.
+    !
+    real(psb_dpk_), intent(in)    :: u
+    real(psb_dpk_), intent(in)    :: L
+    real(psb_dpk_), intent(out)   :: sn,cn,dn
+
+    real(psb_dpk_) :: m
+    m = exp(-2.0_psb_dpk_*DPI*L)
+
+    call sncndn( u, m, sn, cn, dn )
+
+    return
+
+  end subroutine
+
+end submodule
